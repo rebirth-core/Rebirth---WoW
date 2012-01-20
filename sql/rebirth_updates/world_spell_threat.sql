f44aa587c98755cad197e4e0020775b28372b162
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-01-20 16:16:37 +0100

    DB/Spells: Erhöhe Aggro folgender Paladinzauber: - Weihe - Hammer der Rechtschaffenen - Schild der Rechtschaffenen - Schild des Rächers - Exorzismus - Hand der Abrechnung ACHTUNG! Dieser Fix ist nicht Blizzlike! Brauche mehr Daten um es Blizzlike zu machen!

---
+++ sql/rebirth_updates/world_spell_threat.sql
@@ -0,0 +1,8 @@
+REPLACE INTO `spell_threat` (`entry`, `flatMod`, `apPctMod`)
+VALUES 
+('48819', '890', '0.05'),  -- Weihe
+('53595', '610', '0.05'),  -- Hammer der Rechtschaffenen
+('63222', '870', '0.05'),  -- Schild der Rechtschaffenen
+('48827', '990', '0.05'),  -- Schild des Rächers
+('48801', '510', '0.05'),  -- Exorzismus
+('62124', '510', '0.05');  -- Hand der Abrechnung
