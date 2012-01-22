6148e003c3665a209eb691307afa771cd55e2c67
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-01-22 14:03:47 +0100

    CleanUp

--- sql/rebirth_updates/world_spell_target_position.sql
+++
@@ -1,3 +0,0 @@
--- Fixe Portal nach Dalaran (Eiskronzitadelle)
-
-INSERT INTO `spell_target_position` (`id`, `target_map`, `target_position_x`, `target_position_y`, `target_position_z`, `target_orientation`) VALUES (71512, 571, 5807.810059, 588.227783, 660.939087, 1.650267);
