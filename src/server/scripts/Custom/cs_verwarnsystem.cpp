0af9fef18453387cb5f636adf6c1c997be6b7629
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-01-25 16:24:34 +0100

    Core/Verwarnsystem: Füge Funktion hinzu, mit der man Informationen zum Charakter abrufen kann

--- src/server/scripts/Custom/cs_verwarnsystem.cpp
+++ src/server/scripts/Custom/cs_verwarnsystem.cpp
@@ -5,6 +5,38 @@ class warn_commandscript : public CommandScript
     public:
         warn_commandscript() : CommandScript("warn_commandscript") { }
 
+
+
+		static int GetCharInfo(std::string charName, int getInfo)
+		{
+			QueryResult result_char_guid = CharacterDatabase.PQuery("SELECT guid, account, online FROM characters WHERE name = '%s'", charName.c_str());
+			if(result_char_guid)
+			{
+				Field *field = result_char_guid->Fetch();
+				switch(getInfo)
+				{
+					case 1:
+						uint32 char_guid = field[0].GetUInt32();
+						return char_guid;
+					break;
+
+					case 2:
+						uint32 accountId = field[1].GetUInt32();
+						return accountId;
+					break;
+
+					case 3:
+						uint32 IsOnline = field[2].GetUInt32();
+						return IsOnline;
+					break;
+				}
+
+				return 0;
+			}
+		}
+
+
+
         static bool HandleWarnLowCommand(ChatHandler* handler, const char* args)
         {
 			
