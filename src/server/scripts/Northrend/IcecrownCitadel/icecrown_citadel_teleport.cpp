c91147e1779e73f0ccc0a136768b7d0527e3d28e
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-02-03 17:42:50 +0100

    HotFix: Wenn das Gunship Battle deaktiviert wurde, sollen die Spieler sich direkt zu Saurfang porten können

--- src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/icecrown_citadel_teleport.cpp
@@ -30,6 +30,17 @@ class icecrown_citadel_teleport : public GameObjectScript
 
         bool OnGossipHello(Player* player, GameObject* go)
         {
+			QueryResult result = WorldDatabase.PQuery("SELECT disabled FROM unstable_encounters WHERE boss_id = 1");
+			uint32 disabled = 1;
+
+			if(result)
+			{
+			  Field *field = result->Fetch();
+			  disabled = field[0].GetUInt32();
+			}
+			else
+				disabled = 1;
+
             player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to Light's Hammer.", GOSSIP_SENDER_ICC_PORT, LIGHT_S_HAMMER_TELEPORT);
             if (InstanceScript* instance = go->GetInstanceScript())
             {
@@ -37,7 +48,7 @@ class icecrown_citadel_teleport : public GameObjectScript
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Oratory of the Damned.", GOSSIP_SENDER_ICC_PORT, ORATORY_OF_THE_DAMNED_TELEPORT);
                 if (instance->GetBossState(DATA_LADY_DEATHWHISPER) == DONE)
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Rampart of Skulls.", GOSSIP_SENDER_ICC_PORT, RAMPART_OF_SKULLS_TELEPORT);
-                if (instance->GetBossState(DATA_GUNSHIP_EVENT) == DONE)
+                if (instance->GetBossState(DATA_GUNSHIP_EVENT) == DONE || disabled == 1)
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Deathbringer's Rise.", GOSSIP_SENDER_ICC_PORT, DEATHBRINGER_S_RISE_TELEPORT);
                 if (instance->GetData(DATA_COLDFLAME_JETS) == DONE)
                     player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "Teleport to the Upper Spire.", GOSSIP_SENDER_ICC_PORT, UPPER_SPIRE_TELEPORT);
