d3de4c9fc0904f3b8cbd04641a4c27f149c0e1d0
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-02-04 11:08:09 +0100

    Core/Commands: Füge .rebirth event removepoints hinzu

--- src/server/scripts/Custom/event_system.cpp
+++ src/server/scripts/Custom/event_system.cpp
@@ -21,11 +21,27 @@ class rebirth_commandscript : public CommandScript
             return true;
         }
 
+        static bool HandleRemovePointsCommand(ChatHandler* handler, const char* args)
+        {
+            if (!*args)
+               return false;
+
+            int points = atoi((char*)args);
+
+            if(points <  0 || points > 10000)
+               return false;
+
+            LoginDatabase.PExecute("UPDATE account SET event_punkte = event_punkte - %d WHERE id = %u", points, handler->getSelectedPlayer()->GetSession()->GetAccountId());
+
+            return true;
+        }
+
         ChatCommand* GetCommands() const
         {
             static ChatCommand RebirthSubSubCommandTable[] =
             {
                 { "addpoints", SEC_MODERATOR, true, &HandleAddPointsCommand, "", NULL },
+				{ "removepoints", SEC_MODERATOR, true, &HandleRemovePointsCommand, "", NULL },
                 { NULL, 0, false, NULL, "", NULL }
             };
 		
