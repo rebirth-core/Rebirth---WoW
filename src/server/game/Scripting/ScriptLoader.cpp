0097f9495cd4a5045a81d055ed0648d17e424513
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-02-04 11:34:14 +0100

    Core/Commands: Implementiere Commands im ScriptLoader und füge account table update hinzu

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -61,6 +61,7 @@ void AddSC_tele_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
 void AddSC_gps_commandscript();
+void AddSC_rebirth_commandscript();
 
 #ifdef SCRIPTS
 //world
@@ -671,6 +672,7 @@ void AddCommandScripts()
     AddSC_titles_commandscript();
     AddSC_wp_commandscript();
     AddSC_gps_commandscript();
+    AddSC_rebirth_commandscript();
 }
 
 void AddWorldScripts()
