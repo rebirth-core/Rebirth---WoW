a60a833fb26f89df8d63e9105764b9ac32212ddc
Patrick Farnkopf <patrickfarnkopf@gmx.de>
2012-02-08 18:23:41 +0100

    Core/WG: Tausendwinter Stats werden nun in eine Table geschrieben, damit man die Daten via PHP erfassen kann.

---
+++ sql/rebirth_updates/auth/08_02_2012_auth_rebirth_wintergrasp_timer.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `rebirth_wintergrasp_timer`;
+CREATE TABLE `rebirth_wintergrasp_timer` (
+  `id` int(11) NOT NULL,
+  `timer` int(11) NOT NULL DEFAULT '0',
+  `team` int(11) NOT NULL DEFAULT '0',
+  `inWar` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `rebirth_wintergrasp_timer` VALUES ('1', '0', '0', '0');
